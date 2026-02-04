a50e808cdce4f139cc10b556ff4b056823397ae2
sahosskhan <sahosskhan359@gmail.com>
2026-02-05 00:16:38 +0600

    class 2 upload

---
+++ class/swapping.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    // ami akta variable niyasi jeta 5 er value rakhbe
+    // arekta variable niyasi jeta 10 er value rakhbe
+int a = 5;
+int b = 10;
+printf("%d \n",a);
+printf("%d \n",b);
+a=a+b; //a=15
+b=a-b; //b=15-10=5
+a=a-b; //a=15-5=10
+printf("%d \n",a);
+//printf("%d \n",b);
+    return 0;
+}
+// way 2
+//include <stdio.h>
+//int main(){
+//int a = 5;
+//int b = 10;
+//printf("%d \n",a);
+//printf("%d \n",b);
+//int temp;
+//temp = a; //temp=5
+//a = b;    //a=10  
+//b = temp; //b=5
+//printf("%d \n",a);            
+//printf("%d \n",b);
+//    return 0;                 
+
+//}
